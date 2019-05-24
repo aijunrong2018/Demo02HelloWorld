930659cd1a56d89192c9bb184f21d923c7fddece
aijunrong <786394513@qq.com>
2019-05-24 10:09:36 +0800

    修改的文件
    20190524  10:10:00

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,4 +1,3 @@
-
 var appInterface = getApp()
 
 console.log(appInterface.gloubleData)
@@ -6,28 +5,68 @@ console.log(appInterface.gloubleData)
 Page({
 
   clickMe() {
-    this.setData({ showtime: 'hello miss you ' })
-    this.setData({ msg: 'yes I miss you too' })
-  },  
- 
+    this.setData({
+      showtime: 'hello miss you '
+    })
+    this.setData({
+      msg: 'yes I miss you too'
+    })
+  },
+
   data: {
-      showtime: appInterface.gloubleData,
-      array:[{bangding:'hhaha '},{bangding:'dainyixa '}],
-      bangding:'你不点一下吗',
-      array1: ['tomg',2,true,3],
+    showtime: appInterface.gloubleData,
+    array: [{
+      bangding: 'hhaha '
+    }, {
+      bangding: 'dainyixa '
+    }],
+    bangding: '你不点一下吗',
+    array1: ['tomg', 2, true, 3],
 
-      view:'WEBVIEW',
-      '4': 'TT',
-       staffA: { firstName: 'Hulk', lastName: 'Hu' },
-       staffB: { firstName: 'Shang', lastName: 'You' },
-       staffC: { firstName: 'Gideon', lastName: 'Lin' },
-
-    a: 1,
-    b: 2,
-    c: 3,
-    length : 7,
-    test:"nihaoma"
+    view: 'WEBVIEW',
+    '4': 'TT',
+    staffA: {
+      firstName: 'Hulk',
+      lastName: 'Hu'
     },
+    staffB: {
+      firstName: 'Shang',
+      lastName: 'You'
+    },
+    staffC: {
+      firstName: 'Gideon',
+      lastName: 'Lin'
+    },
+
+    //测试缓存用
+    testnum: '12312412412421412' //设置测试参数
+  },
+
+  test: function() {
+    var num = this.data.testnum;
+    wx.setStorage({
+      key: 'testNumber',
+      data: 'num',
+    })
+    this.setData({
+      showtime: num
+    })
+  },
+
+  showactionsheet: function () {
+    wx.showActionSheet({
+      itemList: ['中国', '美国', '日本', '英国'],
+      itemColor: "red",
+      success: function (res) { 
+        if(!res.cancel){
+          console.log(res);
+        }
+      }
+    })
+  },
+
+
+
 
   tapName(event) {
     console.log(event)
